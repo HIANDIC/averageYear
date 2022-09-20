3484f5fadfa536e6bdbb938c61f0b094d40aa7d8
HIANDIC <leaands25@gmail.com>
2022-09-20 14:13:29 +0200

    script modified

--- averageAge.sh
+++ averageAge.sh
@@ -13,7 +13,14 @@ if [[ $age -lt 18 ]]; then
 elif [[ $age -ge 18 ]] && [[ $age -lt 65 ]]; then            
         echo "You are worker"
         echo "$((65 - $age)) years to retire"
-elif [[ $age -ge 65 ]]; then            
-        if [[  ]]
+elif [[ $age -ge 65 ]]; then    
+        # create nested if statement        
+        if [[ $age -lt $ale ]]; then
+                echo "retired"
+                echo "$(($ale - $age)) years to die"
+        elif 
+                echo "Already died!!!"
+
+fi
 
 
